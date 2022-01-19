c2b0d2e4097cf5df43b9c02aa0bf1c04253fbd2d
Alexander Germashov <alexander.germashov@syndicode.com>
2022-01-19 17:04:28 +0200

    upd test(not worked)

--- src/features/Authentication/organisms/SignUpForm/__tests__/SignUpForm.test.js
+++ src/features/Authentication/organisms/SignUpForm/__tests__/SignUpForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {render, screen, waitFor} from '@testing-library/react'
+import {render, screen, waitFor, fireEvent} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { act } from 'react-dom/test-utils';
 import SignUpForm from 'features/Authentication/organisms/SignUpForm';
@@ -8,30 +8,36 @@ import { API_URL } from 'constants/api';
 import { SIGNUP_FIELDS } from 'constants/formTest.constants';
 const client = createClient({ url: API_URL });
 
-
 describe('SignUp Formik form tests', () => {
   it('Success test', async () => {
-    const handleSubmit = jest.fn()
-    await render(
-      <Provider value={client}>
-        <SignUpForm onSubmit={handleSubmit} />
-      </Provider>
-    )
+    const onSubmit = jest.fn()
+    const testValues = {
+      firstName: SIGNUP_FIELDS.FIRST_NAME,
+      lastName: SIGNUP_FIELDS.LAST_NAME,
+      email: SIGNUP_FIELDS.EMAIL,
+      password: SIGNUP_FIELDS.PASSWORD,
+    };
 
-    userEvent.type(screen.getByLabelText(/First name/i), SIGNUP_FIELDS.FIRST_NAME)
-    userEvent.type(screen.getByLabelText(/Last name/i), SIGNUP_FIELDS.LAST_NAME)
-    userEvent.type(screen.getByLabelText(/Email/i), SIGNUP_FIELDS.EMAIL)
-    userEvent.type(screen.getByLabelText(/Password/i), SIGNUP_FIELDS.PASSWORD)
+    act(() => {
+      render(
+        <Provider value={client}>
+          <SignUpForm onSubmit={onSubmit} {...testValues}/>
+        </Provider>
+      )
+    });
 
+    userEvent.type(screen.getByPlaceholderText(/First name/i), testValues.firstName)
+    userEvent.type(screen.getByPlaceholderText(/Last name/i), testValues.lastName)
+    userEvent.type(screen.getByPlaceholderText(/Email/i), testValues.email)
+    userEvent.type(screen.getByPlaceholderText(/Password/i), testValues.password)
     userEvent.click(screen.getByRole('button', {name: /submit/i}))
 
     await waitFor(() => {
-      expect(handleSubmit).toHaveBeenCalledTimes(1);
-      expect(handleSubmit).toHaveBeenCalledWith({
-        firstName: SIGNUP_FIELDS.FIRST_NAME,
-        lastName: SIGNUP_FIELDS.LAST_NAME,
-        email: SIGNUP_FIELDS.EMAIL,
-        password: SIGNUP_FIELDS.PASSWORD,
+      expect(onSubmit).toHaveBeenCalledWith({
+        firstName: testValues.firstName,
+        lastName: testValues.lastName,
+        email: testValues.email,
+        password: testValues.password,
       })
     })
   });
