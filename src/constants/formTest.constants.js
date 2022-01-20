424dd3e3701b1f19017efd03d8ee5775cc7efd71
Alexander Germashov <alexander.germashov@syndicode.com>
2022-01-20 17:48:22 +0200

    removed constants fields

--- src/constants/formTest.constants.js
+++
@@ -1,6 +0,0 @@
-export const AUTH_FIELDS = {
-  FIRST_NAME: 'John',
-  LAST_NAME: 'Dee',
-  EMAIL: 'john.dee@someemail.com',
-  PASSWORD: 'Alex_12345'
-};
--- src/features/Authentication/organisms/SignUpForm/__tests__/SignUpForm.test.js
+++ src/features/Authentication/organisms/SignUpForm/__tests__/SignUpForm.test.js
@@ -2,30 +2,28 @@ import React from 'react';
 import {render, screen, waitFor} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import SignUpForm from 'features/Authentication/organisms/SignUpForm';
-import { AUTH_FIELDS } from 'constants/formTest.constants';
-
 
 describe('SignUp Formik form', () => {
   const dispatch = jest.fn();
-  const renderCOmponent = () => render(<SignUpForm />);
+  const renderComponent = () => render(<SignUpForm />);
 
   describe('with valid data', () => {
     it('dispatch SignUp() with correct params', async () => {
-      renderCOmponent();
+      renderComponent();
 
       //{"data":{"createUser":{"id":"48","__typename":"User"}}}
-      userEvent.type(screen.getByTestId('firstName'), AUTH_FIELDS.FIRST_NAME)
-      userEvent.type(screen.getByTestId('lastName'), AUTH_FIELDS.LAST_NAME)
-      userEvent.type(screen.getByTestId('email'), AUTH_FIELDS.EMAIL)
-      userEvent.type(screen.getByTestId('password'), AUTH_FIELDS.PASSWORD)
+      userEvent.type(screen.getByTestId('firstName'), 'John')
+      userEvent.type(screen.getByTestId('lastName'), 'Dee')
+      userEvent.type(screen.getByTestId('email'), 'john.dee@someemail.com')
+      userEvent.type(screen.getByTestId('password'), 'Alex_12345')
       userEvent.click(screen.getByTestId('submit'))
 
       await waitFor(() => {
         expect(dispatch).toHaveBeenCalledWith({
-          firstName: AUTH_FIELDS.FIRST_NAME,
-          lastName: AUTH_FIELDS.LAST_NAME,
-          email: AUTH_FIELDS.EMAIL,
-          password: AUTH_FIELDS.PASSWORD,
+          firstName: 'John',
+          lastName: 'Dee',
+          email: 'john.dee@someemail.com',
+          password: 'Alex_12345',
         })
       });
     });
